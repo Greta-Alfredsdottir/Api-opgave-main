ad0334fbeb41a5441503080288fb1612beb77926
Greta <asth0010@elev.techcollege.dk>
2025-09-30 09:42:10 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
 // her er vores javascript
+
+fetch('https://dog.ceo/api/breeds/image/random/3')
+  .then(response => response.json())
+  .then(data => {
+    view(data);
+  })
+  .catch(error => {
+    console.error('Fejl:', error);
+  });
+
+function view(data) {
+  const output = document.getElementById('output');
+  output.innerHTML = data.message
+    .map(url => `<img src="${url}" alt="Dog image" style="max-width:200px;">`)
+    .join('');
+}
